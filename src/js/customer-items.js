6f12ef63e35bd7c6ab28eabba4fdb7a81381b8cc
Oksana Donchuk <80112131+OksanaDonchuk@users.noreply.github.com>
2023-12-17 22:59:02 +0200

    chanfe path photo

--- src/js/customer-items.js
+++ src/js/customer-items.js
@@ -1,23 +1,32 @@
+import dimaPreviewUrl from '../img/customer_340.jpg';
+import dimaOriginalUrl from '../img/customer_1280.jpg';
+import shopPreviewUrl from '../img/shop_340.jpg';
+import shopOriginalUrl from '../img/shop_1280.jpg';
+import eqipPreviewUrl from '../img/equipment_340.jpg';
+import eqipOriginalUrl from '../img/equipment_1280.jpg';
+import helpPreviewUrl from '../img/help_340.jpg';
+import helpOriginalUrl from '../img/help_1280.jpg';
+
 export const customerItems = [
   {
-    preview: './img/customer_340.jpg',
-    original: './img/customer_1280.jpg',
+    preview: dimaPreviewUrl,
+    original: dimaOriginalUrl,
     description:
       'Це Дмитро. Він девелопер. За два роки роботи 24/7 втратив спортивну форму. І зрозумів: щось треба робити',
   },
   {
-    preview: './img/shop_340.jpg',
-    original: './img/shop_1280.jpg',
+    preview: shopPreviewUrl,
+    original: shopOriginalUrl,
     description: 'Дмитро побіжав до спортивного магазину',
   },
   {
-    preview: './img/equipment_340.jpg',
-    original: './img/equipment_1280.jpg',
+    preview: eqipPreviewUrl,
+    original: eqipOriginalUrl,
     description: 'Накупив вселякого приладдя. І почав активно займатися',
   },
   {
-    preview: './img/help_340.jpg',
-    original: './img/help_1280.jpg',
+    preview: helpPreviewUrl,
+    original: helpOriginalUrl,
     description:
       'Але щось поганенько виходить без професійних підказок. Дмитру потрібна допомога!',
   },
