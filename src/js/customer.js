17aef1a3ce2c65e2c9dbb581fc3ac99b5669c75e
Oksana Donchuk <80112131+OksanaDonchuk@users.noreply.github.com>
2023-12-17 21:50:31 +0200

    Update customer.js

--- src/js/customer.js
+++ src/js/customer.js
@@ -1,6 +1,31 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { customerItems } from './customer-items.js';
+// import { customerItems } from './customer-items.js';
+
+const customerItems = [
+  {
+    preview: './img/customer_340.jpg',
+    original: './img/customer_1280.jpg',
+    description:
+      'Це Дмитро. Він девелопер. За два роки роботи 24/7 втратив спортивну форму. І зрозумів: щось треба робити',
+  },
+  {
+    preview: './img/shop_340.jpg',
+    original: './img/shop_1280.jpg',
+    description: 'Дмитро побіжав до спортивного магазину',
+  },
+  {
+    preview: './img/equipment_340.jpg',
+    original: './img/equipment_1280.jpg',
+    description: 'Накупив вселякого приладдя. І почав активно займатися',
+  },
+  {
+    preview: './img/help_340.jpg',
+    original: './img/help_1280.jpg',
+    description:
+      'Але щось поганенько виходить без професійних підказок. Дмитру потрібна допомога!',
+  },
+];
 
 const gallery = document.querySelector('.customer');
 
