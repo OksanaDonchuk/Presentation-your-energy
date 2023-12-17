12722dcff749a8b85c0ea27bb3c4134bd500a0fb
OksanaDonchuk <80112131+OksanaDonchuk@users.noreply.github.com>
2023-12-17 20:59:21 +0000

    Deploying to gh-pages from @ OksanaDonchuk/Presentation-your-energy@6f12ef63e35bd7c6ab28eabba4fdb7a81381b8cc 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,10 +1,10 @@
-import"./assets/styles-4df50263.js";import{S as p}from"./assets/vendor-c9def49e.js";const i=[{preview:"./img/customer_340.jpg",original:"./img/customer_1280.jpg",description:"Це Дмитро. Він девелопер. За два роки роботи 24/7 втратив спортивну форму. І зрозумів: щось треба робити"},{preview:"./img/shop_340.jpg",original:"./img/shop_1280.jpg",description:"Дмитро побіжав до спортивного магазину"},{preview:"./img/equipment_340.jpg",original:"./img/equipment_1280.jpg",description:"Накупив вселякого приладдя. І почав активно займатися"},{preview:"./img/help_340.jpg",original:"./img/help_1280.jpg",description:"Але щось поганенько виходить без професійних підказок. Дмитру потрібна допомога!"}],s=document.querySelector(".customer");function m(e){return e.map(({preview:r,original:t,description:o})=>`<li class="customer_items">
+import"./assets/styles-4df50263.js";import{S as n}from"./assets/vendor-c9def49e.js";const o="/Presentation-your-energy/assets/customer_340-c5104b93.jpg",a="/Presentation-your-energy/assets/customer_1280-776e3c55.jpg",c="/Presentation-your-energy/assets/shop_340-67e7963e.jpg",l="/Presentation-your-energy/assets/shop_1280-76c0aa30.jpg",p="/Presentation-your-energy/assets/equipment_340-eace790b.jpg",g="/Presentation-your-energy/assets/equipment_1280-0e6ba9d9.jpg",u="/Presentation-your-energy/assets/help_340-08d0fe53.jpg",m="/Presentation-your-energy/assets/help_1280-fcc8dc8e.jpg",e=[{preview:o,original:a,description:"Це Дмитро. Він девелопер. За два роки роботи 24/7 втратив спортивну форму. І зрозумів: щось треба робити"},{preview:c,original:l,description:"Дмитро побіжав до спортивного магазину"},{preview:p,original:g,description:"Накупив вселякого приладдя. І почав активно займатися"},{preview:u,original:m,description:"Але щось поганенько виходить без професійних підказок. Дмитру потрібна допомога!"}],y=document.querySelector(".customer");function P(r){return r.map(({preview:s,original:t,description:i})=>`<li class="customer_items">
   <a class="customer_link" href="${t}">
     <img
       class="customer_img"
-      src="${r}"
-      alt="${o}"
+      src="${s}"
+      alt="${i}"
     />
   </a>
-</li>`).join("")}console.log(i);s.insertAdjacentHTML("afterbegin",m(i));new p(".customer a",{captionsData:"alt",captionDelay:250});
+</li>`).join("")}console.log(e);y.insertAdjacentHTML("afterbegin",P(e));new n(".customer a",{captionsData:"alt",captionDelay:250});
 //# sourceMappingURL=commonHelpers.js.map
