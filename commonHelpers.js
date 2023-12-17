8de8e3bb4dcca6969ebcefb2d3a78d818ff7836b
OksanaDonchuk <80112131+OksanaDonchuk@users.noreply.github.com>
2023-12-17 19:24:42 +0000

    Deploying to gh-pages from @ OksanaDonchuk/Presentation-your-energy@c5facdacf8f252628b4c3e49f26e45242ae8b3d6 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import"./assets/styles-4df50263.js";import{S as p}from"./assets/vendor-c9def49e.js";const i=[{preview:"./img/customer_340.jpg",original:"./img/customer_1280.jpg",description:"Це Дмитро. Він девелопер. За два роки роботи 24/7 втратив спортивну форму. І зрозумів: щось треба робити"},{preview:"./img/shop_340.jpg",original:"./img/shop_1280.jpg",description:"Дмитро побіжав до спортивного магазину."},{preview:"./img/equipment_340.jpg",original:"./img/equipment_1280.jpg",description:"Накупив вселякого приладдя. І почав активно займатися"},{preview:"./img/help_340.jpg",original:"./img/help_1280.jpg",description:"Але щось поганенько виходить без професійних підказок. Дмитру потрібна допомога!"}],s=document.querySelector(".customer");function m(e){return e.map(({preview:r,original:t,description:o})=>`<li class="customer_items">
+import"./assets/styles-4df50263.js";import{S as s}from"./assets/vendor-c9def49e.js";const i=[{preview:"../../src/img/customer_340.jpg",original:"../../src/img/customer_1280.jpg",description:"Це Дмитро. Він девелопер. За два роки роботи 24/7 втратив спортивну форму. І зрозумів: щось треба робити"},{preview:"./img/shop_340.jpg",original:"./img/shop_1280.jpg",description:"Дмитро побіжав до спортивного магазину."},{preview:"./img/equipment_340.jpg",original:"./img/equipment_1280.jpg",description:"Накупив вселякого приладдя. І почав активно займатися"},{preview:"./img/help_340.jpg",original:"./img/help_1280.jpg",description:"Але щось поганенько виходить без професійних підказок. Дмитру потрібна допомога!"}],p=document.querySelector(".customer");function c(e){return e.map(({preview:r,original:t,description:o})=>`<li class="customer_items">
   <a class="customer_link" href="${t}">
     <img
       class="customer_img"
@@ -6,5 +6,5 @@ import"./assets/styles-4df50263.js";import{S as p}from"./assets/vendor-c9def49e.
       alt="${o}"
     />
   </a>
-</li>`).join("")}console.log(i);s.insertAdjacentHTML("afterbegin",m(i));new p(".customer a",{captionsData:"alt",captionDelay:250});
+</li>`).join("")}console.log(i);p.insertAdjacentHTML("afterbegin",c(i));new s(".customer a",{captionsData:"alt",captionDelay:250});
 //# sourceMappingURL=commonHelpers.js.map
