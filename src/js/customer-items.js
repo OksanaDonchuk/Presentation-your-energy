ef50670ae83127ce524d0c316a86f5c442e6d5f3
Oksana Donchuk <80112131+OksanaDonchuk@users.noreply.github.com>
2023-12-17 21:11:32 +0200

    change path of img

--- src/js/customer-items.js
+++ src/js/customer-items.js
@@ -1,23 +1,23 @@
 export const customerItems = [
   {
-    preview: '../img/customer_340.jpg',
-    original: '../img/customer_1280.jpg',
+    preview: './img/customer_340.jpg',
+    original: './img/customer_1280.jpg',
     description:
       'Це Дмитро. Він девелопер. За два роки роботи 24/7 втратив спортивну форму. І зрозумів: щось треба робити',
   },
   {
-    preview: '../img/shop_340.jpg',
-    original: '../img/shop_1280.jpg',
+    preview: './img/shop_340.jpg',
+    original: './img/shop_1280.jpg',
     description: 'Дмитро побіжав до спортивного магазину.',
   },
   {
-    preview: '../img/equipment_340.jpg',
-    original: '../img/equipment_1280.jpg',
+    preview: './img/equipment_340.jpg',
+    original: './img/equipment_1280.jpg',
     description: 'Накупив вселякого приладдя. І почав активно займатися',
   },
   {
-    preview: '../img/help_340.jpg',
-    original: '../img/help_1280.jpg',
+    preview: './img/help_340.jpg',
+    original: './img/help_1280.jpg',
     description:
       'Але щось поганенько виходить без професійних підказок. Дмитру потрібна допомога!',
   },
