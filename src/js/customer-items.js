b15f996acd18542935002955d6ed79877a4ce2b8
Oksana Donchuk <80112131+OksanaDonchuk@users.noreply.github.com>
2023-12-17 21:38:17 +0200

    Update customer-items.js

--- src/js/customer-items.js
+++ src/js/customer-items.js
@@ -1,14 +1,14 @@
 export const customerItems = [
   {
-    preview: '../../src/img/customer_340.jpg',
-    original: '../../src/img/customer_1280.jpg',
+    preview: './img/customer_340.jpg',
+    original: './img/customer_1280.jpg',
     description:
       'Це Дмитро. Він девелопер. За два роки роботи 24/7 втратив спортивну форму. І зрозумів: щось треба робити',
   },
   {
     preview: './img/shop_340.jpg',
     original: './img/shop_1280.jpg',
-    description: 'Дмитро побіжав до спортивного магазину.',
+    description: 'Дмитро побіжав до спортивного магазину',
   },
   {
     preview: './img/equipment_340.jpg',
